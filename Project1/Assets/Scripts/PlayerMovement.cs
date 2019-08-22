ffa4cec0ce77dfb816cb9ebc0ea09d505aae2f7f
Zishan Ansari <42755217+zansari2@users.noreply.github.com>
2019-08-22 13:38:38 -0400

    Game skeleton

--- Project1/Assets/Scripts/PlayerMovement.cs
+++ Project1/Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
-
     Rigidbody rb;
-    public float speed = 10.0f;
-
-    // Start is called before the first frame update
+    public float speed = 2.0f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)||(Input.GetKey("d")))
-        {
-            rb.velocity = transform.right * speed;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow)||(Input.GetKey("a")))
-        {
-            rb.velocity = -transform.right * speed;
-        }
-        else if (Input.GetKey(KeyCode.UpArrow)||(Input.GetKey("w")))
-        {
-            rb.velocity = new Vector3(0, 0, 1) * speed;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow)||(Input.GetKey("s")))
-        {
-            rb.velocity = new Vector3(0, 0, -1) * speed;
-        }
-        else
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
     }
 }
